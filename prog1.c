d8dc1a08bc87833202f6b97499b8e555ad167c15
Connor Morris <cmorr78@lsu.edu>
2024-09-11 11:59:28 -0500

    Added first program
    Added a hello world program & fixed the .gitignore file.

---
+++ prog1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// This program prints Hello World! to the terminal and then moves the cursor to a newline.
+int main(int argc, char *argv[]) {
+	printf("Hello World!\n");
+	return 0;
+}
